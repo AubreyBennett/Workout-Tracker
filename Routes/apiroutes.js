f70492ffde65f7f3f8e907e20815de4cec07df11
Aubrey Bennett <27aubreyb@gmail.com>
2021-04-18 21:32:43 -0400

    added post route, and removed info from index.js

--- Routes/apiroutes.js
+++ Routes/apiroutes.js
@@ -2,6 +2,15 @@ const { Router } = require("express");
 const workout = require("../Models/workout.js");
 
 module.exports = function (app) {
+    app.post("/api/workouts", function (req, res) {
+        db.workout.create({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch((error) => {
+            res.json(error);
+        });
+    });
+
     app.get("/api/workouts", function (req, res) {
         db.workout.find({})
         .then((dbWorkout) => {
@@ -10,6 +19,8 @@ module.exports = function (app) {
             res.json(error);
         });
     });
+
+
 };
 
 module.exports = Router;
